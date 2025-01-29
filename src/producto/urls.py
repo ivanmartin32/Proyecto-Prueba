f8623b4a482d32a69640b91aa7c0c213dce0cd8a
ivanmartin32 <ivanmartin_32@hotmail.com>
2025-01-29 15:24:34 -0300

    Navegacion entre App

--- src/producto/urls.py
+++ src/producto/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'Producto'
+
 urlpatterns = [
-    path('principal/', views.index, name='index'),
+    path('', views.index, name='index'),
 ]
